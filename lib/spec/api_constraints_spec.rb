f6132abbdf84cb75ba6bbaaa62519e3c6727cbcb
oscarmarcelo95 <oscarmarcelo95@gmail.com>
2015-10-19 11:28:24 -0500

    adds api constraints and apisconstrains spec

---
+++ lib/spec/api_constraints_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe ApiConstraints do
+  let(:api_constraints_v1) { ApiConstraints.new(version: 1) }
+  let(:api_constraints_v2) { ApiConstraints.new(version: 2, default: true) }
+
+  describe 'matches?' do
+    it 'returns true when the verision matches the Accept header' do
+      request = double(host: 'api.apisonrails.dev', headers: { 'Accept' => 'application/vnd.apisonrails.v1'})
+      api_constraints_v1.matches?(request).should be_true
+    end
+
+    it 'return default when no version is specified' do
+      request = double(host: 'api.apisonrails.dev')
+      api_constraints_v2.matches?(request).should be_true
+    end
+  end
+end
